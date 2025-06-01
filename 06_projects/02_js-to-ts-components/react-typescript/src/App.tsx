0ba9253c55d19b1a9f1c0bd83c566a0f2fc5f3cc
chlvk <ochlvk@gmail.com>
2025-06-01 15:49:40 +0300

    setup project

---
+++ 06_projects/02_js-to-ts-components/react-typescript/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return <div className="flex h-full">
+    <aside className="flex h-[100vh] overflow-auto flex-col items-start gap-3 p-3">
+      <button className="button">1</button>
+    </aside>
+    <main className="flex items-center justify-center flex-1 border-l-2">Main</main>
+  </div>
+}
+
+export default App;
